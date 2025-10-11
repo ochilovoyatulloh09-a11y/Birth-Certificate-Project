f371cd40936f608e5e681c74c0b95d4b15547ac2
Oyatullo Ochilov <ochilovoyatulloh09@gmail.com>
2025-10-11 14:07:51 +0300

    Worked on Babies management menu.

--- Birth-Certificate-Project/Program.cs
+++ Birth-Certificate-Project/Program.cs
@@ -1,3 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BirthCertificate.Entities;
+using BirthCertificate.Services;
 
-Console.WriteLine("Hello, World!");
+BabyService babyService = new BabyService();
+BirthCertificateService bcService = new BirthCertificateService();
+
+while (true)
+{
+    Console.WriteLine("Main Menu");
+    Console.WriteLine("1) Manage Babies");
+    Console.WriteLine("2) Manage Birth Certificates");
+    Console.WriteLine("Exit");
+    Console.WriteLine("Choose an option: ");
+
+    string? choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            ManageBabies();
+            break;
+
+
+    }
+
+//----------------------------------------------------------------Manage Baby option-----------------------------------------------------------------------------
+    void ManageBabies()
+    {
+        while (true)
+        {
+            Console.WriteLine("Babies menu: ");
+            Console.WriteLine("1) Create Baby");
+            Console.WriteLine("2) List Of Babies");
+            Console.WriteLine("3) Update Baby");
+            Console.WriteLine("4) Delete Baby");
+            Console.WriteLine("0) Back");
+
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    CreateBaby();
+                    break;
+                case "2":
+                    babyService.ListOfBabies();
+                    break;
+                
+            }
+        }
+    }
+
+//--------------------------------------------------------------Create Baby---------------------------------------------
+    void CreateBaby()
+    {
+        Console.WriteLine("Create new Baby");
+        try
+        {
+            Console.Write("Enter babies ID: ");
+            long id = long.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter babies Name: ");
+            string? name = Console.ReadLine();
+
+            Console.Write("Enter babies Middle name: ");
+            string? middlename = Console.ReadLine();
+
+            Console.Write("Enter babies Nationality: ");
+            string? nationality = Console.ReadLine();
+
+            Console.Write("Enter babies Gender: ");
+            string? gender = Console.ReadLine();
+
+            Console.Write("Enter babies Place of Birth: ");
+            string? placeOfBirth = Console.ReadLine();
+
+            Console.Write("Enter babies Date of Birth DD.MM.YYYY: ");
+            DateTime dateOfBirth = DateTime.Parse(Console.ReadLine()!);
+            babyService.AddBaby(id, name, middlename, nationality, gender, placeOfBirth, dateOfBirth);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
